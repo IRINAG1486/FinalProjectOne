3becdcf9e439babd178002515426d454f80ecf72
IRINAG <ira848@mail.ru>
2023-03-11 20:18:52 +0600

    Wrote method ChangeArray

--- FinalWork_1/Program.cs
+++ FinalWork_1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+
+string [] ChangeArray (string [] array)
+{
+    string [] changedArray = new string [array.Length];
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            changedArray [counter] = array[i];
+            conter++;
+        }
+    }
+    return changedArray;
+}
